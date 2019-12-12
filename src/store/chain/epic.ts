f33b27bf242a461c5d3b63ad29107c13691359df
qiuhao <qiuhao951@gmail.com>
2019-12-13 00:46:04 +0800

    fix: remove all interval in epics

--- src/store/chain/epic.ts
+++ src/store/chain/epic.ts
@@ -36,98 +36,80 @@ export const connectEpic: Epic<RootAction, RootAction, RootState> = action$ =>
 export const fetchPricesFeedEpic: Epic<RootAction, RootAction, RootState> = (action$, state$) =>
     action$.pipe(
         filter(isActionOf(actions.fetchPricesFeed.request)),
-        switchMap(action =>
-            // interval(1000 * 60).pipe(
-            of().pipe(
-                startWith(0),
-                withLatestFrom(state$),
-                filter(([_, state]) => state.chain.app !== null), // ensure has app entity,
-                switchMap(([_, state]) => {
-                    const assetList = action.payload;
-                    const app = state.chain.app;
-                    return combineLatest(assetList.map(asset => app!.query.oracle.values(asset))).pipe(
-                        map(result =>
-                            assetList.map((asset, index) => {
-                                const price = get(result, [index, 'value', 'value'], { isNone: true });
-                                return {
-                                    asset,
-                                    price: u8aToNumber(price),
-                                };
-                            }),
-                        ),
-                        map(actions.fetchPricesFeed.success),
-                    );
-                }),
-            ),
-        ),
+        withLatestFrom(state$),
+        filter(([action, state]) => state.chain.app !== null), // ensure has app entity,
+        switchMap(([action, state]) => {
+            const assetList = action.payload;
+            const app = state.chain.app;
+            return combineLatest(assetList.map(asset => app!.query.oracle.values(asset))).pipe(
+                map(result =>
+                    assetList.map((asset, index) => {
+                        const price = get(result, [index, 'value', 'value'], { isNone: true });
+                        return {
+                            asset,
+                            price: u8aToNumber(price),
+                        };
+                    }),
+                ),
+                map(actions.fetchPricesFeed.success),
+            );
+        }),
     );
 
 export const fetchVaultsEpic: Epic<RootAction, RootAction, RootState> = (action$, state$) =>
     action$.pipe(
         filter(isActionOf(actions.fetchVaults.request)),
-        switchMap(action =>
-            // interval(10000 * 60).pipe(
-            of().pipe(
-                startWith(0),
-                withLatestFrom(state$),
-                filter(([_, state]) => state.chain.app !== null),
-                switchMap(([_, state]) => {
-                    const assetList = action.payload;
-                    const app = state.chain.app;
-                    return combineLatest(
-                        assetList.map(asset =>
-                            combineLatest([
-                                app!.query.cdpEngine.debitExchangeRate(asset),
-                                app!.query.cdpEngine.liquidationPenalty(asset),
-                                app!.query.cdpEngine.liquidationRatio(asset),
-                                app!.query.cdpEngine.maximumTotalDebitValue(asset),
-                                app!.query.cdpEngine.requiredCollateralRatio(asset),
-                                app!.query.cdpEngine.stabilityFee(asset),
-                            ]),
-                        ),
-                    ).pipe(
-                        map(result => {
-                            return assetList.map((asset, index) => ({
-                                asset,
-                                debitExchangeRate: u8aToNumber(result[index][0]),
-                                liquidationPenalty: u8aToNumber(result[index][1]),
-                                liquidationRatio: u8aToNumber(result[index][2]),
-                                maximumTotalDebitValue: u8aToNumber(result[index][3]),
-                                requiredCollateralRatio: u8aToNumber(result[index][4]),
-                                stabilityFee: u8aToNumber(result[index][5]),
-                            }));
-                        }),
-                        map(actions.fetchVaults.success),
-                    );
+        withLatestFrom(state$),
+        filter(([action, state]) => state.chain.app !== null),
+        switchMap(([action, state]) => {
+            const assetList = action.payload;
+            const app = state.chain.app;
+            return combineLatest(
+                assetList.map(asset =>
+                    combineLatest([
+                        app!.query.cdpEngine.debitExchangeRate(asset),
+                        app!.query.cdpEngine.liquidationPenalty(asset),
+                        app!.query.cdpEngine.liquidationRatio(asset),
+                        app!.query.cdpEngine.maximumTotalDebitValue(asset),
+                        app!.query.cdpEngine.requiredCollateralRatio(asset),
+                        app!.query.cdpEngine.stabilityFee(asset),
+                    ]),
+                ),
+            ).pipe(
+                map(result => {
+                    return assetList.map((asset, index) => ({
+                        asset,
+                        debitExchangeRate: u8aToNumber(result[index][0]),
+                        liquidationPenalty: u8aToNumber(result[index][1]),
+                        liquidationRatio: u8aToNumber(result[index][2]),
+                        maximumTotalDebitValue: u8aToNumber(result[index][3]),
+                        requiredCollateralRatio: u8aToNumber(result[index][4]),
+                        stabilityFee: u8aToNumber(result[index][5]),
+                    }));
                 }),
-            ),
-        ),
+                map(actions.fetchVaults.success),
+            );
+        }),
     );
 
 export const fetchTotalIssuance: Epic<RootAction, RootAction, RootState> = (action$, state$) =>
     action$.pipe(
         filter(isActionOf(actions.fetchTotalIssuance.request)),
-        switchMap(action =>
-            // interval(1000 * 60).pipe(
-            of().pipe(
-                startWith(0),
-                withLatestFrom(state$),
-                filter(([_, state]) => state.chain.app !== null), // ensure has app entity,
-                switchMap(([_, state]) => {
-                    const assetList = action.payload;
-                    const app = state.chain.app;
-                    return combineLatest(assetList.map(asset => app!.query.tokens.totalIssuance(asset))).pipe(
-                        map(result =>
-                            assetList.map((asset, index) => {
-                                return {
-                                    asset,
-                                    issuance: u8aToNumber(result[index]),
-                                };
-                            }),
-                        ),
-                        map(actions.fetchTotalIssuance.success),
-                    );
-                }),
-            ),
-        ),
+        withLatestFrom(state$),
+        filter(([action, state]) => state.chain.app !== null), // ensure has app entity,
+        switchMap(([action, state]) => {
+            const assetList = action.payload;
+            const app = state.chain.app;
+            return combineLatest(assetList.map(asset => app!.query.tokens.totalIssuance(asset))).pipe(
+                map(result =>
+                    assetList.map((asset, index) => {
+                        return {
+                            asset,
+                            issuance: u8aToNumber(result[index]),
+                        };
+                    }),
+                ),
+                map(actions.fetchTotalIssuance.success),
+            );
+        }),
     );
--- src/store/user/epic.ts
+++ src/store/user/epic.ts
@@ -13,28 +13,23 @@ import { startLoading, endLoading } from '../loading/reducer';
 export const fetchAssetBalanceEpic: Epic<RootAction, RootAction, RootState> = (action$, state$) =>
     action$.pipe(
         filter(isActionOf(actions.fetchAssetsBalance.request)),
-        switchMap(action =>
-            interval(1000 * 60).pipe(
-                startWith(0),
-                withLatestFrom(state$),
-                filter(([_, state]) => state.chain.app !== null),
-                filter(([_, state]) => state.user.account !== null),
-                switchMap(([_, state]) => {
-                    const data = action.payload as AssetList;
-                    const app = state.chain.app;
-                    const account = state.user.account!.address;
-                    return combineLatest(data.map(asset => app!.query.tokens.balance(asset, account))).pipe(
-                        map(result => {
-                            return data.map((asset, index) => ({
-                                asset,
-                                balance: u8aToNumber(result[index]),
-                            }));
-                        }),
-                        map(actions.fetchAssetsBalance.success),
-                    );
+        withLatestFrom(state$),
+        filter(([_, state]) => state.chain.app !== null),
+        filter(([_, state]) => state.user.account !== null),
+        switchMap(([action, state]) => {
+            const data = action.payload as AssetList;
+            const app = state.chain.app;
+            const account = state.user.account!.address;
+            return combineLatest(data.map(asset => app!.query.tokens.balance(asset, account))).pipe(
+                map(result => {
+                    return data.map((asset, index) => ({
+                        asset,
+                        balance: u8aToNumber(result[index]),
+                    }));
                 }),
-            ),
-        ),
+                map(actions.fetchAssetsBalance.success),
+            );
+        }),
     );
 
 export const importAccmountEpic: Epic<RootAction, RootAction, RootState> = action$ =>
@@ -54,41 +49,36 @@ export const importAccmountEpic: Epic<RootAction, RootAction, RootState> = actio
 export const fetchVaultsEpic: Epic<RootAction, RootAction, RootState> = (action$, state$) =>
     action$.pipe(
         filter(isActionOf(actions.fetchVaults.request)),
-        switchMap(action =>
-            interval(60 * 1000).pipe(
-                startWith(0),
-                withLatestFrom(state$),
-                filter(([_, state]) => state.chain.app !== null),
-                filter(([_, state]) => state.user.account !== null),
-                switchMap(([_, state]) => {
-                    const app = state.chain.app!;
-                    const account = state.user.account!;
-                    const assetList = action.payload;
-                    return combineLatest(
-                        assetList.map(asset =>
-                            combineLatest([
-                                app.query.vaults.collaterals(account.address, asset),
-                                app.query.vaults.debits(account.address, asset),
-                            ]),
-                        ),
-                    ).pipe(
-                        map(result => {
-                            return assetList
-                                .map((asset, index) => ({
-                                    asset: asset,
-                                    collateral: u8aToNumber(result[index][0]),
-                                    debit: u8aToNumber(result[index][1]),
-                                }))
-                                .filter(item => {
-                                    // filter empty vaults
-                                    return item.collateral !== 0 || item.debit !== 0;
-                                });
-                        }),
-                        map(actions.fetchVaults.success),
-                        startWith(startLoading(actions.FETCH_VAULTS)),
-                        endWith(endLoading(actions.FETCH_VAULTS)),
-                    );
+        withLatestFrom(state$),
+        filter(([_, state]) => state.chain.app !== null),
+        filter(([_, state]) => state.user.account !== null),
+        switchMap(([action, state]) => {
+            const app = state.chain.app!;
+            const account = state.user.account!;
+            const assetList = action.payload;
+            return combineLatest(
+                assetList.map(asset =>
+                    combineLatest([
+                        app.query.vaults.collaterals(account.address, asset),
+                        app.query.vaults.debits(account.address, asset),
+                    ]),
+                ),
+            ).pipe(
+                map(result => {
+                    return assetList
+                        .map((asset, index) => ({
+                            asset: asset,
+                            collateral: u8aToNumber(result[index][0]),
+                            debit: u8aToNumber(result[index][1]),
+                        }))
+                        .filter(item => {
+                            // filter empty vaults
+                            return item.collateral !== 0 || item.debit !== 0;
+                        });
                 }),
-            ),
-        ),
+                map(actions.fetchVaults.success),
+                startWith(startLoading(actions.FETCH_VAULTS)),
+                endWith(endLoading(actions.FETCH_VAULTS)),
+            );
+        }),
     );
