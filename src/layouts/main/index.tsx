c4d86d351243740327b028579a06795aafa58f7b
Bryan Chen <xlchen1291@gmail.com>
2020-01-23 13:30:21 +1300

    fix import

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles, createStyles } from '@material-ui/core';
 import { Theme } from '@material-ui/core/styles';
-import acalaTypes from '@acala-network/types/src/interfaces/runtime/definitions';
+import acalaTypes from '@acala-network/types/interfaces/runtime/definitions';
 
 import { loadingSelector } from '@/store/loading/reducer';
 import Loading from '@/components/loading';
