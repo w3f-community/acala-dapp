257b4f60a495ea37085d54da5a9abb44021fc5e2
qiuhao <qiuhao951@gmail.com>
2020-02-17 18:31:40 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c1957bf65e9277c1a28b82ae496ebb0.js"
+  "/precache-manifest.7c6387eac27483dfccc43fab8f347a04.js"
 );
 
 self.addEventListener('message', (event) => {
